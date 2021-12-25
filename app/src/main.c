f006f82238fdf36026d74ffb27c3002beabcf0dc
PQH111 <769753451@qq.com>
2021-12-25 15:58:42 +0800

    同步测试

--- app/src/main.c
+++ app/src/main.c
@@ -22,9 +22,9 @@ int main(void)
 
     while (1)
     {
-        rt_pin_write(LED0_PIN, PIN_HIGH);
-        rt_thread_mdelay(500);
-        rt_pin_write(LED0_PIN, PIN_LOW);
-        rt_thread_mdelay(500);
+//        rt_pin_write(LED0_PIN, PIN_HIGH);
+//        rt_thread_mdelay(500);
+//        rt_pin_write(LED0_PIN, PIN_LOW);
+//        rt_thread_mdelay(500);
     }
 }
